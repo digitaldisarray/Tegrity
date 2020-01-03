7febf48a829477baf60e75ce211c591e19adff49
Kevin <electrichexagon8@gmail.com>
2020-01-02 20:22:24 -0800

    Usability and functioning save button

--- Tegrity/src/xyz/disarray/Launcher.java
+++ Tegrity/src/xyz/disarray/Launcher.java
@@ -1,5 +1,6 @@
 package xyz.disarray;
 
+import xyz.disarray.tegrity.Standalone3;
 import xyz.disarray.tegrity.Tegrity;
 
 public class Launcher {
@@ -7,7 +8,9 @@ public class Launcher {
 	public final static double VERSION = 0.1;
 	
 	public static void main(String[] args) {
-		TEGRITY = new Tegrity();
-		TEGRITY.run();
+		//TEGRITY = new Tegrity();
+		//TEGRITY.run();
+		
+		new Standalone3().run();
 	}
 }
--- Tegrity/src/xyz/disarray/tegrity/TegrityGUI.java
+++ Tegrity/src/xyz/disarray/tegrity/TegrityGUI.java
@@ -1,6 +1,5 @@
 package xyz.disarray.tegrity;
 
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.SystemColor;
 import java.awt.datatransfer.DataFlavor;
