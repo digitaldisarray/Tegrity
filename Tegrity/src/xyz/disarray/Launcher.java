005b7dce387aeb5671fd3e6b4d6e461e80878e66
Kevin <electrichexagon8@gmail.com>
2020-01-05 22:00:58 -0800

    Some bad code

--- Tegrity/src/xyz/disarray/Launcher.java
+++ Tegrity/src/xyz/disarray/Launcher.java
@@ -5,16 +5,30 @@ import xyz.disarray.tegrity.Standalone;
 public class Launcher {
 	public static Standalone STANDALONE;
 	public final static double VERSION = 0.1;
-	
+
 	public static void main(String[] args) {
-		
-		if(args.length == 0) {
+
+		if (args.length == 0) {
 			STANDALONE = new Standalone();
 			STANDALONE.run();
 		} else {
 			// Parse args
+
+			// Variables to store assigned tasks
+			boolean loadDb = false;
+			String db = null;
+			boolean check = false;
+
+			for (int i = 0; i < args.length; i++) {
+				if (args[i].equals("-db") && i < args.length - 1) {
+					loadDb = true;
+					db = args[i + 1];
+				}
+			}
+
 			// Do stuff
+
 		}
-		
+
 	}
 }
